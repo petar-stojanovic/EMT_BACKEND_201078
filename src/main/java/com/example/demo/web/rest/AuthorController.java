825cdf9e4f0d0adb1b1e6333cef6ed0f6e7c8f08
Petar <2001petarstojanovic@gmail.com>
2023-03-27 19:58:50 +0200

    added author Controller

---
+++ src/main/java/com/example/demo/web/rest/AuthorController.java
@@ -0,0 +1,28 @@
+package com.example.demo.web.rest;
+
+import com.example.demo.model.Author;
+import com.example.demo.service.AuthorService;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/authors")
+@CrossOrigin(origins = "http://localhost:3000")
+public class AuthorController {
+
+    private final AuthorService authorService;
+
+    public AuthorController(AuthorService authorService) {
+        this.authorService = authorService;
+    }
+
+    @GetMapping
+    public List<Author> findAll() {
+        return this.authorService.findAll();
+    }
+
+}
